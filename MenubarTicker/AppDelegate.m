6691b8698fb594922068fe0fd67f3968ad76c5e5
Zandr Martin <zandrmartin@gmail.com>
2017-11-11 22:12:03 -0500

    remove rdio since it no longer exists

--- MenubarTicker/AppDelegate.m
+++ MenubarTicker/AppDelegate.m
@@ -9,7 +9,6 @@
 #import "AppDelegate.h"
 
 #import "iTunes.h"
-#import "Rdio.h"
 #import "Spotify.h"
 
 const NSTimeInterval kPollingInterval = 10.0;
@@ -18,7 +17,6 @@ const NSTimeInterval kPollingInterval = 10.0;
 @interface AppDelegate ()
 
 @property (nonatomic, retain) iTunesApplication *iTunes;
-@property (nonatomic, retain) RdioApplication *rdio;
 @property (nonatomic, retain) SpotifyApplication *spotify;
 
 @property (nonatomic, retain) NSStatusItem *statusItem;
@@ -30,7 +28,6 @@ const NSTimeInterval kPollingInterval = 10.0;
 @implementation AppDelegate
 
 @synthesize iTunes;
-@synthesize rdio;
 @synthesize spotify;
 
 @synthesize statusItem;
@@ -42,15 +39,14 @@ const NSTimeInterval kPollingInterval = 10.0;
     [[NSDistributedNotificationCenter defaultCenter] removeObserver:self name:nil object:nil];
 
     self.iTunes = nil;
-    self.rdio = nil;
     self.spotify = nil;
-    
+
     self.statusItem = nil;
     self.statusMenu = nil;
-    
+
     [self.timer invalidate];
     self.timer = nil;
-    
+
     [super dealloc];
 }
 
@@ -61,17 +57,12 @@ const NSTimeInterval kPollingInterval = 10.0;
                                                 selector:@selector(timerDidFire:)
                                                 userInfo:nil
                                                  repeats:YES];
-    
+
     [[NSDistributedNotificationCenter defaultCenter] addObserver:self
                                                         selector:@selector(didReceivePlayerNotification:)
                                                             name:@"com.apple.iTunes.playerInfo"
                                                           object:nil];
-    
-    [[NSDistributedNotificationCenter defaultCenter] addObserver:self
-                                                        selector:@selector(didReceivePlayerNotification:)
-                                                            name:@"com.rdio.desktop.playStateChanged"
-                                                          object:nil];
-    
+
     [[NSDistributedNotificationCenter defaultCenter] addObserver:self
                                                         selector:@selector(didReceivePlayerNotification:)
                                                             name:@"com.spotify.client.PlaybackStateChanged"
@@ -81,14 +72,13 @@ const NSTimeInterval kPollingInterval = 10.0;
 - (void)awakeFromNib
 {
     self.iTunes = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
-    self.rdio = [SBApplication applicationWithBundleIdentifier:@"com.rdio.desktop"];
     self.spotify = [SBApplication applicationWithBundleIdentifier:@"com.spotify.client"];
-    
+
     self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     self.statusItem.menu = self.statusMenu;
     self.statusItem.highlightMode = YES;
     self.statusItem.toolTip = @"Menu Bar Ticker";
-    
+
     [self updateTrackInfo];
 }
 
@@ -96,11 +86,9 @@ const NSTimeInterval kPollingInterval = 10.0;
 - (void)updateTrackInfo
 {
     id currentTrack = nil;
-    
+
     if ([self.iTunes isRunning] && [self.iTunes playerState] == iTunesEPlSPlaying) {
         currentTrack = [self.iTunes currentTrack];
-    } else if ([self.rdio isRunning] && [self.rdio playerState] == RdioEPSSPlaying) {
-        currentTrack = [self.rdio currentTrack];
     } else if ([self.spotify isRunning] && [self.spotify playerState] == SpotifyEPlSPlaying) {
         currentTrack = [self.spotify currentTrack];
     }
